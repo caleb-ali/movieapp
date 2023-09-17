01ff3db60f7c309b394f3e0423457528a04f784b
caleb-ali <slimkally@gmail.com>
2023-09-17 11:37:55 +0100

    fix

--- utils/requests.js
+++ utils/requests.js
@@ -1,9 +1,10 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+
 export const getTrendingMovies = async () => {
   try {
-    const res = await fetch(`${BASE_URL}/trending/movie/day?language=en-US&api_key=${API_KEY}`);
+    const res = await fetch(`${BASE_URL}/movie/top_rated?language=en-US&api_key=${API_KEY}`);
     if (!res.ok) {
       throw new Error('Network response was not ok');
     }
