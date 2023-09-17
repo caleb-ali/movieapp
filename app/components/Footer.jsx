c43fa2587916487d7dee59da86afc03f8e022d93
caleb-ali <slimkally@gmail.com>
2023-09-17 02:17:48 +0100

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div id="footer">
+      <div id='socials'>
+        <img src='/facebook.png'/>
+        <img src='/instagram.png'/>
+        <img src='twitter.png'/>
+        <img src='youtube.png'/>
+      </div>
+      <div id='footerlinks'>
+        <p>Conditions of Use</p>
+        <p>Privacy & Policy</p>
+        <p>Press Room</p>
+      </div>
+      <div className='text-gray-500' id='copyright'>
+        <p> &copy; 2021 MovieBox by Adriana Eka Prayudha</p>
+      </div>
+    </div>
   )
 }
 
